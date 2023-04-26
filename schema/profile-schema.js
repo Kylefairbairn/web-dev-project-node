4255647adcc98d3063ecba579386d1a12baa63c1
kylefairbairn <kyle.fairbairn@gmail.com>
2023-04-26 19:39:55 -0400

    updated

--- schema/profile-schema.js
+++ schema/profile-schema.js
@@ -4,16 +4,20 @@ import mongoose from "mongoose";
 const profileSchema = new mongoose.Schema(
     {
             user: { type: mongoose.Schema.Types.ObjectId, ref:"User", required: true, unique: true},
-            score: { type: Number, required: true, unique: false },
-            gamesPlayed: { type: Number, required: true, unique: false },
-            totalQuestions: { type: Number, required: true, unique: false },
+            score: { type: Number, required: false, unique: false },
+            gamesPlayed: { type: Number, required: false, unique: false },
+            totalQuestions: { type: Number, required: false, unique: false },
             category: [
                     {
-                            category: { type: String, required: true },
-                            freq: { type: Number, required: true }
+                            category: { type: String, required: false },
+                            freq: { type: Number, required: false
+
+
+
+                            }
                     }
             ],
-            points: {type: Number, required: true, unique: false}
+            points: {type: Number, required: false, unique: false}
     },
     { collection: "profile"}
 );
